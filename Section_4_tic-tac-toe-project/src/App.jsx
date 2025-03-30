0be4a74b35643ae0136363d95f05b8d6a4e455d4
m0c0zv5 <mani.chekuri@walmart.com>
2025-03-30 14:11:34 -0500

    Started tic tac toe game

---
+++ Section_4_tic-tac-toe-project/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+    return (
+        <>
+            <main>
+                <div id="game-container">
+                    <ol id='players'>
+                        <li>
+                            <span className='player-name'>Player 1</span>
+                            <span className='symbol'>X</span>
+                        </li>
+                        <li>
+                            <span className='player-name'>Player 2</span>
+                            <span className='symbol'>O</span>
+                        </li>
+
+                    </ol>
+                    PLAYERS
+                    GAMEBOARD
+                </div>
+                LOGS
+            </main>
+        </>
+    )
+}
+
+export default App

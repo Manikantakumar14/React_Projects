61abf062e9672053a793c65b51270f43a608cf1f
m0c0zv5 <mani.chekuri@walmart.com>
2025-03-31 22:22:36 -0500

    continuing tic-tac-toe

--- Section_4_tic-tac-toe-project/src/App.jsx
+++ Section_4_tic-tac-toe-project/src/App.jsx
@@ -5,12 +5,18 @@ function App() {
                 <div id="game-container">
                     <ol id='players'>
                         <li>
+                            <span className='player'>
                             <span className='player-name'>Player 1</span>
                             <span className='symbol'>X</span>
+                            </span>
+                            <button>Edit</button>
                         </li>
                         <li>
+                            <span className='player'>
                             <span className='player-name'>Player 2</span>
                             <span className='symbol'>O</span>
+                            </span>
+                            <button>Edit</button>
                         </li>
 
                     </ol>

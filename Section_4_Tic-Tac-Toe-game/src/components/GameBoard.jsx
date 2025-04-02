30d0b3a1924f2b45e49f2284580ad45f1c1beaf3
m0c0zv5 <mkumar.chekuri@gmail.com>
2025-04-01 23:20:05 -0500

    continuing tic-tac-toe

--- Section_4_Tic-Tac-Toe-game/src/components/GameBoard.jsx
+++ Section_4_Tic-Tac-Toe-game/src/components/GameBoard.jsx
@@ -1,3 +1,7 @@
+import {useState} from "react";
+
+import {useState} from 'react';
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -6,6 +10,16 @@ const initialGameBoard = [
 
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        })
+    }
+
     return <ol id='game-board'>
         {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>

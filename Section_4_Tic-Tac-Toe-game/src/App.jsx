ea7a6830430597d1a351a064a29006f56a9427c6
m0c0zv5 <mkumar.chekuri@gmail.com>
2025-04-27 23:20:22 -0500

    continuing tic tac toe

--- Section_4_Tic-Tac-Toe-game/src/App.jsx
+++ Section_4_Tic-Tac-Toe-game/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     const [gameTurns, setGameTurns] = useState([]);
     const activePlayer = deriveActivePlayer(gameTurns);
 
-    let gameBoard = initialGameBoard;
+    let gameBoard = [...initialGameBoard.map(array => [...array])];
 
     for (const turn of gameTurns) {
         const {square, player} = turn;
@@ -57,6 +57,10 @@ function App() {
         });
     }
 
+    function handleRestart() {
+        setGameTurns([]);
+    }
+
     return (
         <>
             <main>
@@ -67,7 +71,7 @@ function App() {
                         <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
 
                     </ol>
-                    {(winner || hasDraw) && <GameOver winner={winner}/>}
+                    {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
                     <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
                 </div>
                 <Log turns={gameTurns}/>

4447404aa2c75936e8521f19070e33c2bd85e439
m0c0zv5 <mkumar.chekuri@gmail.com>
2025-04-27 22:53:14 -0500

    continuing tic tac toe

--- Section_4_Tic-Tac-Toe-game/src/App.jsx
+++ Section_4_Tic-Tac-Toe-game/src/App.jsx
@@ -32,7 +32,15 @@ function App() {
         gameBoard[row][col] = player;
     }
 
+    let winner;
     for (const combination of WINNING_COMBINATIONS) {
+        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+        if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+            winner = firstSquareSymbol;
+        }
 
     }
 
@@ -56,6 +64,7 @@ function App() {
                         <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
 
                     </ol>
+                    {winner && <p>You won, {winner}!</p>}
                     <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
                 </div>
                 <Log turns={gameTurns}/>

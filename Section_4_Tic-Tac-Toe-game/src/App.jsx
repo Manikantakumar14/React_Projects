f385aa6dfb4adbd79b2c457a5a33002199f3542d
m0c0zv5 <mkumar.chekuri@gmail.com>
2025-04-27 23:03:40 -0500

    continuing tic tac toe

--- Section_4_Tic-Tac-Toe-game/src/App.jsx
+++ Section_4_Tic-Tac-Toe-game/src/App.jsx
@@ -3,6 +3,7 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import {WINNING_COMBINATIONS} from "./winning-combinations.js";
+import GameOver from "./components/GameOver.jsx";
 
 const initialGameBoard = [
     [null, null, null],
@@ -44,6 +45,8 @@ function App() {
 
     }
 
+    const hasDraw = gameTurns.length === 9 && !winner
+
     function handleSelectSquare(rowIndex, colIndex) {
         //setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
 
@@ -64,7 +67,7 @@ function App() {
                         <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
 
                     </ol>
-                    {winner && <p>You won, {winner}!</p>}
+                    {(winner || hasDraw) && <GameOver winner={winner}/>}
                     <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
                 </div>
                 <Log turns={gameTurns}/>
